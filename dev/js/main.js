de314cd40f9d5194c5e9fa6db21da808c922a9b0
Nuk <i@xuzhao.net>
2015-05-07 17:56:58 -0400

    Added IE8 workaround

--- dev/js/main.js
+++ dev/js/main.js
@@ -189,7 +189,28 @@ function clearCookie() {
 }
 
 $(document).ready(function() {
-    
+    if (!Array.prototype.indexOf)
+    {
+        Array.prototype.indexOf = function(elt /*, from*/)
+        {
+            var len = this.length >>> 0;
+
+            var from = Number(arguments[1]) || 0;
+            from = (from < 0)
+                ? Math.ceil(from)
+                : Math.floor(from);
+            if (from < 0)
+                from += len;
+
+            for (; from < len; from++)
+            {
+                if (from in this &&
+                    this[from] === elt)
+                    return from;
+            }
+            return -1;
+        };
+    }
     // clearCookie();
     gen_article_array(getCookie("siteRead"));
     var index = getRandomNewsFromAvailable();
